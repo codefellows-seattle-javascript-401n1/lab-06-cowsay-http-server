3eb64582792062360cf9c8e840b66ee355f31a40
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-05-15 14:36:17 -0700

    refactored out different ways to handle get and post responses

--- lab-taylor/server.js
+++ lab-taylor/server.js
@@ -3,18 +3,24 @@
 const http = require('http');
 const parseUrl = require('url').parse;
 const parseQuery = require('querystring').parse;
-const bodyParser = require('./lib/parse-json');
+const bodyParser = require('./lib/parse-req');
 const cowsay = require('cowsay');
 
 const port = process.argv[2] || 3000;
+const badReq = 'bad request\ntry: localhost:3000/cowsay?text=howdy';
 
-function buildRes(data, res) {
+function buildCowRes(data, status, res) {
+  res.writeHead(status, {
+    'Content-Type': 'text/plain'
+  });
   res.write(cowsay.say({text: data.toString()}));
   res.end();
 }
 
 function defaultRes(res){
-  res.writeHead(200);
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
   res.write('API ENDPOINTS:\n/cowsay');
   res.end();
 }
@@ -25,30 +31,29 @@ const server = http.createServer(function (req,res) {
 
   if (req.url.path === '/') {
     defaultRes(res);
+    return;
   }
 
   if (req.url.pathname === '/cowsay') {
-    bodyParser(req).then(function () {
-
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
+    if (req.method === 'POST') {
+      bodyParser.handlePost(req).then(function () {
+        buildCowRes(req.body.text, 200, res);
+      }).catch(function (err) {
+        console.error('err', err);
+        buildCowRes(badReq, 400, res);
       });
-      buildRes(req.body.text, res);
-    }).catch(function (err) {
-      console.log('err', err);
-
-      res.writeHead(400, {
-        'Content-Type':'text/plain'
+    } else if (req.method === 'GET'){
+      bodyParser.handleGet(req).then(function() {
+        buildCowRes(req.body.text, 200, res);
+      }).catch(function(err){
+        console.error('err', err);
+        buildCowRes(badReq, 400, res);
       });
-      buildRes('bad request\ntry: localhost:3000/cowsay?text=howdy', res);
-    });
+    }
+  } else {
+    buildCowRes(badReq, 400, res);
   }
 
-
-  // 1. detect path
-  // 2. if `/` then respond with endpoints
-  // 3. if `/cowsay` then detect method
-  // 4. process depending on method.
 });
 
 server.listen(port, function () {
