602bd1fe49f9a359cd8202b4af1707f24f112b82
Doug Popadince <gsmatth@icloud.com>
2016-05-15 15:26:04 -0700

    added pathname parsing for '/' and cowsay

--- lab-doug/server.js
+++ lab-doug/server.js
@@ -1,18 +1,23 @@
 const http = require('http');
-// const parseurl = require('url').parse;
-// const parsequery = require('queryString').parse;
-// const cowsay = require('cowsay');
+const parseUrl = require('url').parse;
+const parseQuery = require('querystring').parse;
+//const cowsay = require('cowsay');
 
 const port = process.argv[2] || 3000;
 
 const server = http.createServer(function(req, res){
-  console.log(req);
+    req.url = parseUrl(req.url);
+    req.url.query = parseQuery(req.url.query);
+
+  if(req.url.pathname == '/'){
+    console.log('log pathname: ' + req.url.pathname);
+  }
+  if(req.url.pathname == '/cowsay'){
+    console.log('log pathname: ' + req.url.pathname);
+  }
+  //if GET and pathname is /cowsay then response is =
 });
-server.listen(port, function(){
-  console.log('server running on port: ' + port);
-});
-/*
-Create a body parser that uses Promises to parse the json in the body of POST, PUT, or DELETE requests
-*/
-  // req.url = parseUrl(req.url);
-  // req.url.query = parseQuery(req.url.query);
+
+  server.listen(port, function(){
+    console.log('server running on port: ' + port);
+  });
