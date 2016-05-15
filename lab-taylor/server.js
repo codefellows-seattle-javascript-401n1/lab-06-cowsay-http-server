cc039559691b9d4cdba0ab15ed7ca356aa9b42ef
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-05-15 13:37:51 -0700

    basic server & gulp-nodemon

--- lab-taylor/server.js
+++ lab-taylor/server.js
@@ -1,3 +1,22 @@
 'use strict';
 
 const http = require('http');
+const parseUrl = require('url').parse;
+const parseQuery = require('querystring').parse;
+const cowsay = require('cowsay');
+
+const port = process.argv[2] || 3000;
+
+const server = http.createServer(function (req,res) {
+  req.url = parseUrl(req.url);
+  req.query = parseQuery(req.query);
+
+  // 1. detect path
+  // 2. if `/` then respond with endpoints
+  // 3. if `/cowsay` then detect method
+  // 4. process depending on method.
+});
+
+server.listen(port, function () {
+  console.log(`server is running on port ${port}`);
+});
