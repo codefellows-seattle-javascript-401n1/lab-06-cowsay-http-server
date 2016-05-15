8a88f0c29efe1b6bcec37dc5d71507509614d73a
Doug Popadince <gsmatth@icloud.com>
2016-05-15 14:46:19 -0700

    added basic http server

--- lab-doug/gulpfile.js
+++ lab-doug/gulpfile.js
@@ -4,9 +4,8 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('eslint', () => {
-  return
   gulp.src(['**/*.js', '!node_modules/**'])
-   .pipe.(eslint());
+   .pipe(eslint())
    .pipe(eslint.format())
    .pipe(eslint.failAfterError());
 });
--- lab-doug/server.js
+++ lab-doug/server.js
@@ -1,22 +1,18 @@
 const http = require('http');
-const parseurl = require('url').parse;
-const parsequery = require('queryString').parse;
-const cowsay = require('cowsay');
+// const parseurl = require('url').parse;
+// const parsequery = require('queryString').parse;
+// const cowsay = require('cowsay');
 
 const port = process.argv[2] || 3000;
 
 const server = http.createServer(function(req, res){
-
-  req.url = parseUrl(req.url);
-  req.url.query = parseQuery(req.url.query);
-
-  res.writeHead(200);
-  res.write('hello from basic http server');
-  res.end();
-
-  console.log('req url ', req.url);
+  console.log(req);
 });
-
 server.listen(port, function(){
-  console.log('soemthing');
+  console.log('server running on port: ' + port);
 });
+/*
+Create a body parser that uses Promises to parse the json in the body of POST, PUT, or DELETE requests
+*/
+  // req.url = parseUrl(req.url);
+  // req.url.query = parseQuery(req.url.query);
