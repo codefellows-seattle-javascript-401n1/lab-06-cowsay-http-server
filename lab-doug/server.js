56dc4118ded12fa96ed5dfe9dc6d37533e3e3dda
Doug Popadince <gsmatth@icloud.com>
2016-05-15 13:33:21 -0700

    initial commit with project scaffolding

---
+++ lab-doug/server.js
@@ -0,0 +1,21 @@
+onst http = require('http');
+const parseurl = require('url').parse;
+const parsequery = require('queryString').parse;
+
+const port = process.argv[2] || 3000;
+
+const server = http.createServer(function(req, res){
+
+  req.ur = parseUrl(req.url);
+  req.url.query = parseQuery(req.url.query);
+
+  res.writeHead(200);
+  res.write('hello from basic http server');
+  res.end();
+
+  console.log('req url ', req.url);
+});
+
+server.listen(port, function(){
+  console.log('soemthing');
+});
