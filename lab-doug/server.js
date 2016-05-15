5592f1dafc43c29333ecadb40f3c061453eb823c
Doug Popadince <gsmatth@icloud.com>
2016-05-15 16:06:49 -0700

    added successful req.url.query if statemetn to detect query on cowsay get

--- lab-doug/server.js
+++ lab-doug/server.js
@@ -14,12 +14,24 @@ const server = http.createServer(function(req, res){
     res.write('API Endpoints:/api/cowsay');
     res.end();
     console.log('log pathname: ' + req.url.pathname);
-// res: Content-Type: text/plain
-// status code of 200
-// res.write('API Endpoints:/api/cowsay'))
   }
   if(req.url.pathname == '/cowsay'){
-    console.log('log pathname: ' + req.url.pathname);
+    if (req.method == 'GET'){
+      console.log('cowsay pathname: ' + req.url.pathname);
+      console.log('cowsay method is GET');
+    } if (req.url.query){
+      console.log('query string does exist');
+    }
+
+// the query string should have the key value text=<message>
+// the response header should include Content-Type: text/plain
+// if the query text=messsage is set, respond with:
+// a status code of 200
+// a body including the value returned from cowsay.say({text: <querystring text>})
+// if the query text=message is not set, respond with:
+// status code = 400
+// a body including the value returned from cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'})
+
   }
 
 });
