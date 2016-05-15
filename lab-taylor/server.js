c2e7d0a42cbd95f61c77c588e5870fadfea6bdb5
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-05-15 14:19:11 -0700

    everything working now I tihnk

--- lab-taylor/server.js
+++ lab-taylor/server.js
@@ -3,20 +3,45 @@
 const http = require('http');
 const parseUrl = require('url').parse;
 const parseQuery = require('querystring').parse;
+const bodyParser = require('./lib/parse-json');
 const cowsay = require('cowsay');
 
 const port = process.argv[2] || 3000;
 
+function buildRes(data, res) {
+  res.write(cowsay.say({text: data.toString()}));
+  res.end();
+}
+
+function defaultRes(res){
+  res.writeHead(200);
+  res.write('API ENDPOINTS:\n/cowsay');
+  res.end();
+}
+
 const server = http.createServer(function (req,res) {
   req.url = parseUrl(req.url);
-  req.query = parseQuery(req.query);
-
-  console.log('req', req.url);
+  req.url.query = parseQuery(req.url.query);
 
   if (req.url.path === '/') {
-    res.writeHead(200);
-    res.write('API ENDPOINTS:\n/cowsay');
-    res.end();
+    defaultRes(res);
+  }
+
+  if (req.url.pathname === '/cowsay') {
+    bodyParser(req).then(function () {
+
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      buildRes(req.body.text, res);
+    }).catch(function (err) {
+      console.log('err', err);
+
+      res.writeHead(400, {
+        'Content-Type':'text/plain'
+      });
+      buildRes('bad request\ntry: localhost:3000/cowsay?text=howdy', res);
+    });
   }
 
 
