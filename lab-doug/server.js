f84bc1dc07035d882e020bca995d5a2e5552617b
Doug Popadince <gsmatth@icloud.com>
2016-05-16 11:14:59 -0700

    refactored the '/' GET function

--- lab-doug/server.js
+++ lab-doug/server.js
@@ -10,29 +10,46 @@ const server = http.createServer(function(req, res){
   req.url = parseUrl(req.url);
   req.url.query = parseQuery(req.url.query);
 
+
   if(req.url.pathname == '/'){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write('API Endpoints:/api/cowsay');
     res.end();
   }
-  if(req.url.pathname == '/cowsay') {
-    if (req.method == 'GET' && req.url.query['text'] === 'saywat'){
+  if(req.url.pathname == '/cowsay' && req.method == 'GET' ) {
+
+    if (req.url.query['text'] === 'saywat'){
+      console.log('GET req header: ', req.headers);
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.write(cowsay.say({text : req.url.query['text']}));
       res.end();
 
-    } else if (req.method == 'GET') {
+    } else {
       res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write(cowsay.say({text: ''}));
+      res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=saywat'}));
       res.end();
     }
-    if(req.url.pathname == '/cowsay') {
-      if (req.method == 'POST' && req.url.query['text'] === 'userdata'){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(cowsay.say({text : req.url.query['text']}));
-        res.end();
-
   }
+
+  if(req.url.pathname == '/cowsay' && req.method == 'POST') {
+    console.log('POST req header: ', req.headers);
+    if(req.headers['content-type'] == 'application/json'){
+      console.log('the POST json header detection worked');
+    }
+  }
+    //   if (req.url.query['text'] === 'userdata'){
+    //     if(req.url.query['Content-Type'] == 'application/json {text: userdata}'){
+    //     res.writeHead(200, {'Content-Type': 'text/plain'});
+    //     res.write(cowsay.say({text : req.url.query['text']}));
+    //     res.end();
+    //   } else {
+    //       res.writeHead(400, {'Content-Type': 'text/plain'});
+    //       res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=userdata'}));
+    //       res.end();
+    //     }
+    //   }
+    // }
+});
 //   POST REQUEST
 //
 // the query string should have the key value text=<message>
@@ -44,7 +61,7 @@ const server = http.createServer(function(req, res){
 // status code = 400
 // a body including the value returned from cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'})
 
-});
+
 
 server.listen(port, function(){
   console.log('server running on port: ' + port);
