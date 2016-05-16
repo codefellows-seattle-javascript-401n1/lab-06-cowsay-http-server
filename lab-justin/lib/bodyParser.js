430f06b2b27633e7b41650ffc9b6ce1e9945a619
%%GITNAME%% <%%GITEMAIL%%>
2016-05-15 20:48:13 -0700

    http server not working

---
+++ lab-justin/lib/bodyParser.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(req){
+  return new Promise(function(resolve, reject){
+    req.body = '';
+    req.on('data', function(data){
+      req.body +=data.toString();
+    });
+
+    req.on('end', function(){
+      try {
+        // req.body = JSON.parse(req.body);
+        resolve();
+        // throw new err('whaa~?');
+      }catch(err){
+        reject(err);
+
+      }
+    });
+  });
+};
