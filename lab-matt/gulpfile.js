c7305256594de067e6f7d3cd1f7ff3df68619a38
Matthew Smith <mattsmith203@hotmail.com>
2016-05-29 15:02:14 -0700

    cleaned up package.json file and added watch task in gulp

--- lab-matt/gulpfile.js
+++ lab-matt/gulpfile.js
@@ -2,11 +2,23 @@
 
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const nodemon = require('gulp-nodemon');
 
 gulp.task('lint', function(){
   return gulp.src(['**/*.js','!node_modules/**'])
   .pipe(eslint()).pipe(eslint.format());
 });
 
+gulp.task('watch', () => {
+  gulp.watch(['**/*.js', '!node_modules/**'], ['lint']);
+  nodemon({
+    script: 'server.js',
+    ext: 'html js'
+  })
+   .on('restart', () => {
+     console.log('restarting server');
+   });
+});
 
-gulp.task('default', ['lint']);
+
+gulp.task('default', ['lint', 'watch']);
