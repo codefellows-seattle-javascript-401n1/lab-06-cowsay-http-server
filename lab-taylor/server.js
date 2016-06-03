9c44489ac934c30c2c033a4e7a14f2ab554555f8
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-06-02 17:50:41 -0700

    display proper error text

--- lab-taylor/server.js
+++ lab-taylor/server.js
@@ -7,7 +7,8 @@ const bodyParser = require('./lib/parse-req');
 const cowsay = require('cowsay');
 
 const port = process.argv[2] || 3000;
-const badReq = {text: 'bad request\ntry: localhost:3000/cowsay?text=howdy&face=ghostbusters', face:'ghostbusters'};
+const error400 = {text: 'bad request\ntry: localhost:3000/api/cowsay?text=howdy&face=ghostbusters', face:'ghostbusters'};
+const error404 = {text: 'route not found\ntry: localhost:3000/api/cowsay?text=howdy&face=ghostbusters', face:'ghostbusters'};
 
 function buildCowRes(options, status, res) {
   res.writeHead(status, {
@@ -39,24 +40,24 @@ const server = http.createServer(function (req,res) {
     return;
   }
 
-  if (req.url.pathname === 'api/cowsay') {
+  if (req.url.pathname === '/api/cowsay') {
     if (req.method === 'POST') {
       bodyParser.handlePost(req).then(function () {
         buildCowRes(req.body, 200, res);
       }).catch(function (err) {
         console.error('err', err);
-        buildCowRes(badReq, 400, res);
+        buildCowRes(error400, 400, res);
       });
     } else if (req.method === 'GET'){
       bodyParser.handleGet(req).then(function() {
         buildCowRes(req.body, 200, res);
       }).catch(function(err){
         console.error('err', err);
-        buildCowRes(badReq, 400, res);
+        buildCowRes(error400, 400, res);
       });
     }
   } else {
-    buildCowRes(badReq, 404, res);
+    buildCowRes(error404, 404, res);
   }
 
 });
